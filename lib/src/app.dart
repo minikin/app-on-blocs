f46fba45a6eb3e8a093533d1cdf1704d021f0a79
Sasha Prokhorenko <djminikin@gmail.com>
2019-02-04 15:23:52 +0100

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_blocs/src/screens/sign_in.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SignIn(),
+    );
+  }
+}
---
+++ lib/src/screens/sign_in.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.blue[100],
+          child: Text('SignIn'),
+        ),
+      ),
+    );
+  }
+}
