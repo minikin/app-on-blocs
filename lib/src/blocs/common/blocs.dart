f689c3286105827beedf1f505c33600d7af35a9b
Sasha Prokhorenko <djminikin@gmail.com>
2019-02-05 10:37:32 +0100

    Update exports

---
+++ lib/src/blocs/common/blocs.dart
@@ -0,0 +1,4 @@
+export 'package:flutter_blocs/src/blocs/common/disposable.dart';
+export 'package:flutter_blocs/src/blocs/common/provider.dart';
+export 'package:flutter_blocs/src/blocs/common/bloc_provider.dart';
+export 'package:flutter_blocs/src/blocs/common/state_transform.dart';
